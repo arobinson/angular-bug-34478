ac07b0e952d274534acbad155393da737a57f3a0
Andrew Robinson <93213+arobinson@users.noreply.github.com>
2019-12-19 09:11:45 -0700

    Fix route

--- test-app/src/app/app-routing.module.ts
+++ test-app/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', loadChildren: () => import('./test-comp/test-comp.module').then((m) => m.TestCompModule) },
+  { path: 'test-comp', loadChildren: () => import('./test-comp/test-comp.module').then((m) => m.TestCompModule) },
   { path: 'test-comp2', loadChildren: () => import('./test-comp2/test-comp2.module').then((m) => m.TestComp2Module) }
 ];
 
